20c7180676bd2b0db878e39d910e2cdc30ec5d60
Anita <a.nita@hotmail.co.uk>
2015-07-23 16:02:20 +0100

    pre-final version

--- js/main.js
+++ js/main.js
@@ -1,16 +1,10 @@
 (function() {
-
-
-
     $(function() {
-
         $("#home a:contains('Home')").parent().addClass('active');
         $("#products a:contains('Product Range')").parent().addClass('active');
         $("#where a:contains('Where to Buy')").parent().addClass('active');
         $("#retailer a:contains('Become a Retailer')").parent().addClass('active');
         $("#contact a:contains('Contact Us')").parent().addClass('active');
-
-
 
         $('#desktop-navigation').affix({
             offset: {
@@ -23,7 +17,5 @@
 
         var d = new Date();
         $('.js_year').text(d.getFullYear());
-
     });
-
 })();
